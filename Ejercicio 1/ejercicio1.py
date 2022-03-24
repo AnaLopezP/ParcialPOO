6d1acaf5e7f6318c16e7dd5058343a4ccb7f2476
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-24 08:49:16 +0100

    creacion clase libro con setters

---
+++ Ejercicio 1/ejercicio1.py
@@ -0,0 +1,31 @@
+from mailbox import NoSuchMailboxError
+
+
+class Libro:
+    def __init__(self, paginas, tapa,nombre, autor, genero, isbn):
+        self.paginas = paginas
+        self.tapa = tapa
+        self.nombre= nombre
+        self.autor = autor
+        self.genero = genero
+        self.isbn = isbn
+
+    def set_nombre(self,nombre):
+        self.nombre = nombre
+
+    def set_paginas(self,paginas):
+        self.paginas = paginas
+
+    def set_tapa(self,tapa):
+        self.tapa = tapa
+
+    def set_autor(self,autor):
+        self.autor = autor
+    
+    def set_genero(self,genero):
+        self.genero = genero
+
+    def set_isbn(self,isbn):
+        self.isbn = isbn
+    
+    
