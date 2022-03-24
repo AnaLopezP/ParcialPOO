4a86adc4131109588d69bd2bd105c7427327ea25
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-24 09:28:53 +0100

    creacion penalizador

--- Ejercicio 4/plazo_fijo.py
+++ Ejercicio 4/plazo_fijo.py
@@ -5,3 +5,9 @@ class plazo_fijo(cuenta_bancaria.cuenta_bnc):
         self.fecha_vencimiento = fecha_vencimiento
         super().__init_subclass__(self, id, nombre, fecha_ap, num_cuenta, saldo)
     
+    def get_fecha_vencimiento(self):
+        return self.fecha_vencimiento
+    def set_fecha_vencimiento(self, fecha_vencimiento):
+        self.fecha_vencimiento = fecha_vencimiento
+    def penalizador(self, fecha_vencimiento, saldo):
+        pass
