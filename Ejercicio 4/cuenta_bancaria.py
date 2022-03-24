23e3f11023dc1f9ae98dd8385cbd6d6b9e8a8de2
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-24 09:24:36 +0100

    cambio nombre crear plazo fijo

---
+++ Ejercicio 4/cuenta_bancaria.py
@@ -0,0 +1,56 @@
+class cuenta_bnc:
+    def __init__(self, id, nombre, fecha_ap, num_cuenta, saldo):
+        self.id = id
+        self.nombre = nombre
+        self.fecha_ap = fecha_ap
+        self.num_cuenta = num_cuenta
+        self.saldo = saldo
+
+    def get_id(self):
+        return self.id
+
+    def get_nombre(self):
+        return self.nombre
+    
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def get_fecha(self):
+        return self.fecha_ap
+
+    def get_numcuenta(self):
+        return self.num_cuenta
+
+    def get_saldo(self):
+        return self.saldo
+
+    def retirar(self, num_cuenta, saldo):
+        print("¿cual es su numero de cuenta?")
+        num_cuenta = str(input())
+        print("¿Que cantidad de dinero quieres retirar?")
+        respuesta = int(input())
+        if respuesta > saldo:
+            print("No es posible.")
+        else: 
+            saldo = saldo - respuesta
+
+    def ingresar(self, saldo):
+        print("¿Que cantidad de dinero quiere ingresar?")
+        respuesta = int(input())
+        saldo = saldo + respuesta
+    
+    def transferir(self, saldo, saldo_recibe, num_recibe, num_retira):
+        print("¿cual es su numero de cuenta?")
+        num_retira = (int(input()))
+        print("A que numero de cuenta quiere realizar latransaccion?")
+        print("Introduzca el num de cuenta")
+        num_recibe = int(input())
+        print("¿Cuanto dinero quiere transferir?")
+        respuesta = int(input())
+        if respuesta > saldo:
+            print("No es posible realizar la transferencia")
+        else:
+            cuenta_bnc.retirar(saldo, num_retira)
+            cuenta_bnc.ingresar(saldo_recibe, num_recibe)
+
+
---
+++ Ejercicio 4/plazo_fijo.py
@@ -0,0 +1,7 @@
+import cuenta_bancaria
+
+class plazo_fijo(cuenta_bancaria.cuenta_bnc):
+    def __init_subclass__(self, id, nombre, fecha_ap, num_cuenta, saldo, fecha_vencimiento):
+        self.fecha_vencimiento = fecha_vencimiento
+        super().__init_subclass__(self, id, nombre, fecha_ap, num_cuenta, saldo)
+    
